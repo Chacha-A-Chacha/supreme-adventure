231b4c8eda8f66553f341ec7edd980fac3521f32
Chacha Chacha <calex2607@gmail.com>
2024-12-19 21:49:41 +0300

    modified:   front-end/src/services/jobService.jsx

--- front-end/src/services/jobService.jsx
+++ front-end/src/services/jobService.jsx
@@ -1,2 +1,14 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../config';
+
+// Fetch all jobs
+export const getJobs = () => axios.get(`${API_BASE_URL}/in-house/jobs`);
+
+// Create a new job
+export const createJob = (data) => axios.post(`${API_BASE_URL}/in-house/new_job`, data);
+
+// Update an existing job (partial update)
+export const updateJob = (jobId, data) => axios.patch(`${API_BASE_URL}/in-house/update_job/${jobId}`, data);
+
+// View single job details by ID
+export const getJobById = (jobId) => axios.get(`${API_BASE_URL}/in-house/job/${jobId}`);
