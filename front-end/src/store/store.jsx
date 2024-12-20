d4d6d56b50e5dcabe5693c1912610e6b62a71701
Chacha Chacha <calex2607@gmail.com>
2024-12-20 06:42:16 +0300

    Update store.jsx

--- front-end/src/store/store.jsx
+++ front-end/src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import materialReducer from './slices/materialSlice';
+import jobReducer from './slices/jobSlice';
 
 const store = configureStore({
   reducer: {
     materials: materialReducer,
+    jobs: jobReducer,
   },
 });
 
-export default  store;
+export default store;
